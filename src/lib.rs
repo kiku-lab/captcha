341acbff804060f9be74900e636b03df09baaa49
mc_fdc <Masato.04.11.2007@gmail.com>
2023-04-03 01:21:09 +0000

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,31 @@ use rusttype::{Font, Scale};
 use wasm_bindgen::prelude::*;
 
 fn generate(text: String) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-    let mut img = ImageBuffer::new(1024, 512);
-    for pixel in img.pixels_mut() {
-        *pixel = Rgb([255, 255, 255])
-    }
-    let font_size = 32;
-    let font_data = include_bytes!("../fonts/captcha.ttf");
+    let mut img = ImageBuffer::from_pixel(1024, 512, Rgb::<u8>([255, 255, 255]));
+    let font_size = 256;
+    let font_data = include_bytes!("./captcha.ttf");
     let font = Font::try_from_bytes(font_data).unwrap();
     let color = Rgb([0, 0, 0]);
-    /* 
     let scale = Scale {
         x: font_size as f32,
         y: font_size as f32,
     };
-    draw_text_mut(&mut img, color, 10, 10, scale, &font, &text);
-    */
-    // img.save("hi.png").unwrap();
+    draw_text_mut(&mut img, color, 256, 128, scale, &font, &text);
+    // Add some noise
+    for _ in 0..1000 {
+        let x = rand::random::<u32>() % 1024;
+        let y = rand::random::<u32>() % 512;
+        img.put_pixel(x, y, Rgb([0, 0, 0]));
+    }
+    // Add distortion to text
+    for _ in 0..10 {
+        let x = rand::random::<u32>() % 1024;
+        let y = rand::random::<u32>() % 512;
+        let x2 = rand::random::<u32>() % 1024;
+        let y2 = rand::random::<u32>() % 512;
+        let color = img.get_pixel(x, y);
+        img.put_pixel(x2, y2, *color);
+    }
     img
 }
 
