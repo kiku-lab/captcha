3a6f2f455559c9f2a899166ddb914573a766ad48
mc_fdc <Masato.04.11.2007@gmail.com>
2023-04-05 10:27:39 +0000

    fix: みにくくした

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ use wasm_bindgen::prelude::*;
 use rand::Rng;
 
 fn generate(text: String) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-    let mut img = ImageBuffer::from_pixel(512, 256, Rgb::<u8>([255, 255, 255]));
-    let font_size = 128;
+    let mut img = ImageBuffer::from_pixel(256, 128, Rgb::<u8>([255, 255, 255]));
+    let font_size = 64;
     let font_data = include_bytes!("./captcha.ttf");
     let font = Font::try_from_bytes(font_data).unwrap();
     let color = Rgb([0, 0, 0]);
@@ -14,26 +14,25 @@ fn generate(text: String) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
         x: font_size as f32,
         y: font_size as f32,
     };
-    let mut last_x = 128;
+    let mut last_x = 32;
     let mut rng = rand::thread_rng();
     for c in text.chars() {
-        let y = rng.gen_range(1..63);
+        let y = rng.gen_range(1..85);
         draw_text_mut(&mut img, color, last_x, y, scale, &font, &c.to_string());
-        last_x += 60;
+        last_x += 32;
     }
-    // draw_text_mut(&mut img, color, 128, 64, scale, &font, &text);
     // Add some noise
     for _ in 0..1000 {
-        let x = rand::random::<u32>() % 512;
-        let y = rand::random::<u32>() % 256;
+        let x = rand::random::<u32>() % 256;
+        let y = rand::random::<u32>() % 128;
         img.put_pixel(x, y, Rgb([0, 0, 0]));
     }
     // Add distortion to text
     for _ in 0..10 {
-        let x = rand::random::<u32>() % 512;
-        let y = rand::random::<u32>() % 256;
-        let x2 = rand::random::<u32>() % 512;
-        let y2 = rand::random::<u32>() % 256;
+        let x = rand::random::<u32>() % 256;
+        let y = rand::random::<u32>() % 128;
+        let x2 = rand::random::<u32>() % 256;
+        let y2 = rand::random::<u32>() % 128;
         let color = img.get_pixel(x, y);
         img.put_pixel(x2, y2, *color);
     }
